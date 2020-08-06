f8bc518fa6cee4c510fb611257f5895d956d13a5
benmc29 <benmc29@live.co.uk>
2020-08-07 00:11:47 +0100

    Added basket functionality

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -54,19 +54,5 @@ namespace MyShop.WebUI.Controllers
             }
 
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
