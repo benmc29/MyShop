3de5c0556cd4ac394923e330a0ac8c71649fea46
benmc29 <benmc29@live.co.uk>
2020-08-13 11:29:50 +0100

    Added order model

--- MyShop/MyShop.Core/Contracts/IBasketService.cs
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -12,6 +12,8 @@ namespace MyShop.Core.Contracts
     {
         void AddToBasket(HttpContextBase httpContext, string productId);
         void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        void ClearBasket(HttpContextBase httpContext);
+
         List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
         BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
 
